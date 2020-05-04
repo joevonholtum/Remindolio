692e789fee48879c175091bbaefe680a46c7352f
Joe <joevh914@gmail.com>
2020-05-04 16:09:44 -0500

    Add project files.

---
+++ Remindolio/Main.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+
+type Reminder = { 
+    Name: string
+    Text: string
+}
+
+let firstReminder = { Name = "first"; Text = "This is the first reminder"; }
+
+let initialize (mainWindow:Window) =
+    let thisTest = mainWindow.FindName("test") :?> TextBlock
+    thisTest.Text <- firstReminder.Text
+    
+
+[<STAThread>]
+[<EntryPoint>]
+let main(_) =
+    let application = Application.LoadComponent(new Uri("App.xaml", UriKind.Relative)) :?> Application
+
+    application.Activated
+    |> Event.add (fun _ -> initialize application.MainWindow)
+
+    application.Run()
